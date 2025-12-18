ac4e9bdf5512d66b2530e4812c2087cc65758468
Harsh Gohil <gohilharsh129@gmail.com>
2025-12-18 15:36:21 +0530

    First Commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import LatestListings from "../components/LatestListings";
+import Plans from "../components/Plans";
+import CTA from "../components/CTA";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <LatestListings />
+      <Plans />
+      <CTA />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
